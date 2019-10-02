4f6af3ba9b5de46733218e0e7c111478bd8b8fd8
Cesar Lopez <cesarl@outlook.com>
2019-10-01 20:58:37 -0600

    PHP and HTML5

--- Index.php
+++ Index.php
@@ -1,3 +1,15 @@
-<?php
-    echo "Hi there!";
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        echo "Hi there!";
+        echo 10+5; 
+    ?>
+</body>
+</html>
