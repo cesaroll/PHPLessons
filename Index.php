fd27edf5673fe71af2e8dd03e51fac5b743a1c56
Cesar Lopez <cesarl@outlook.com>
2019-10-03 14:04:36 -0600

    Include documents in php

--- Index.php
+++ Index.php
@@ -1,3 +1,9 @@
+<?php
+
+    include 'functions/user-functions.php';
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,18 +14,19 @@
 </head>
 <body>
 
+    <header>
+        <nav>
+            <ul>
+                <li><a href="">nav1</a></li>
+                <li><a href="">nav2</a></li>
+                <li><a href="">nav3</a></li>
+                <li><a href="">nav4</a></li>
+            </ul>
+        </nav>
+    </header>
+
     <?php
-        
-        newCalc(100);
-        newCalc(200);
-
-        function newCalc($x){
-            $newnr = $x * 0.75;
-            echo "<br/>Here is 75% of ".$x." : ".$newnr;
-        }
-
-        
-
+        include 'MainContent.php';
     ?>
 
 </body>
---
+++ MainContent.php
@@ -0,0 +1,5 @@
+<section>
+        <div class="someMainContent">
+            <h1>Hi there!</h1>
+        </div>
+</section>
